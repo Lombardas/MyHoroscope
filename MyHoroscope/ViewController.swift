cebaea52f200d2f1acb87bcf2c6d46a9a98ea9d7
Anastasio Almansa <at.almansa@gmail.com>
2023-05-05 13:05:45 +0100

    Initial Commit

---
+++ MyHoroscope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHoroscope
+//
+//  Created by Anastasio Del Toro Almansa on 05/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
